8cc9f7455384df62d789c0eac001561b27d384e3
peter2500zz <2797880090@qq.com>
2026-01-15 11:21:03 +0800

    feat!(log): 改善lua日志行为

--- src/mods/log.rs
+++ src/mods/log.rs
@@ -5,11 +5,33 @@ use super::LuaRegistration;
 
 macro_rules! create_log_func {
     ($lua:expr, $log_table:expr, $name:expr, $macro:ident) => {{
-        let func = $lua.create_function(|_, args: LuaVariadic<LuaValue>| {
-            let message: String = args
-                .iter()
-                .map(|v| v.to_string().unwrap_or_else(|_| format!("{:?}", v)))
-                .collect();
+        let func = $lua.create_function(|lua, args: LuaMultiValue| {
+            let mut message = String::new();
+            let mut formatted = false;
+
+            // 检查参数数量和第一个参数类型
+            if let Some(first) = args.iter().next() {
+                if let LuaValue::String(_) = first {
+                    // 如果有多个参数，且第一个是字符串，尝试调用 string.format
+                    if args.len() > 1 {
+                        let string_format: LuaFunction = lua
+                            .globals()
+                            .get::<LuaTable>("string")?
+                            .get("format")?;
+
+                        message = string_format.call::<String>(&args)?;
+                        formatted = true;
+                    }
+                }
+            }
+
+            if !formatted {
+                message = args
+                    .iter()
+                    .map(|v| v.to_string().unwrap_or_else(|_| format!("{:?}", v)))
+                    .collect::<Vec<_>>()
+                    .join(" ");
+            }
 
             $macro!("{}", message);
             Ok(())
@@ -20,9 +42,7 @@ macro_rules! create_log_func {
 
 inventory::submit! {
     LuaRegistration(|lua| {
-
         let globals = lua.globals();
-
         let log_table = lua.create_table()?;
 
         create_log_func!(lua, log_table, "trace", trace);
@@ -32,7 +52,6 @@ inventory::submit! {
         create_log_func!(lua, log_table, "error", error);
 
         globals.set("Log", log_table)?;
-
         Ok(())
     })
 }
--- types/rumia/class/log.lua
+++ types/rumia/class/log.lua
@@ -2,8 +2,8 @@
 
 ---将日志写入到终端中
 ---@class Log
----@field info fun(...: any) @输出信息级别日志
----@field warn fun(...: any) @输出警告级别日志
----@field error fun(...: any) @输出错误级别日志
----@field debug fun(...: any) @输出调试级别日志
----@field trace fun(...: any) @输出追踪级别日志
+---@field info fun(format: string, ...: any) @输出信息级别日志
+---@field warn fun(format: string, ...: any) @输出警告级别日志
+---@field error fun(format: string, ...: any) @输出错误级别日志
+---@field debug fun(format: string, ...: any) @输出调试级别日志
+---@field trace fun(format: string, ...: any) @输出追踪级别日志
